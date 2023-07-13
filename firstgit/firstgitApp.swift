d8faf81647d0cb9bf13c0b7af5ef4ce15b562360
Rithika <rithu427@gmail.com>
2023-07-13 13:28:54 -0500

    Initial Commit

---
+++ firstgit/firstgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstgitApp.swift
+//  firstgit
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct firstgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
